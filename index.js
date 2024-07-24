f9ad1cccca90e5e240c601a6f8252afed34210c4
kundan175 <kundansingh957632@gmail.com>
2024-07-24 13:41:20 +0530

    cors

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-require('dotenv').config();
-const { swaggerUi, specs } = require('./config/swagger');
+const express = require("express");
+const bodyParser = require("body-parser");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+require("dotenv").config();
+const { swaggerUi, specs } = require("./config/swagger");
 
-const cors = require('cors');
+const cors = require("cors");
 connectDB();
 const app = express();
 
+// const corsOptions = {
+//   origin: ['https://www.poweredbyorange.ai', 'http://localhost'],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', ""],
+//   credentials: true
+// };
 
-
-
-
-
-const corsOptions = {
-  origin: ['https://www.poweredbyorange.ai', 'http://localhost'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-  credentials: true
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 // app.use(cors)
 // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.use(bodyParser.json());
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
