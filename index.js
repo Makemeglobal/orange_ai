5d263fc4720fbd322ed61e8fb952adc3c7b1ad36
kundan175 <kundansingh957632@gmail.com>
2024-08-04 18:08:25 +0530

    bugs fix

--- index.js
+++ index.js
@@ -7,51 +7,47 @@ const { swaggerUi, specs } = require("./config/swagger");
 const Stripe = require("stripe");
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 const cors = require("cors");
-const { stripePaymentStatus } = require("./controller/authController");
 
 connectDB();
 const app = express();
 
-// General middlewares
+// Middleware for general JSON parsing and CORS
 app.use(cors());
 app.use(bodyParser.json()); // This is used for parsing JSON bodies for other routes
 
-// Route-specific middleware for raw body
+// Webhook route
 app.post(
   "/api/auth/webhook",
   express.raw({ type: "application/json" }), // Use express.raw to get the raw body for verification
   (request, response) => {
-    let event = request.body;
     const endpointSecret = process.env.END_POINT_SECRET;
+    const signature = request.headers["stripe-signature"];
+    console.log("Raw body:", request.body.toString());
+    try {
+      const event = stripe.webhooks.constructEvent(
+        request.body, // Raw body here
+        signature,
+        endpointSecret
+      );
 
-    if (endpointSecret) {
-      const signature = request.headers["stripe-signature"];
-      try {
-        event = stripe.webhooks.constructEvent(
-          request.body, // Raw body here
-          signature,
-          endpointSecret
-        );
-      } catch (err) {
-        console.log(`⚠️  Webhook signature verification failed.`, err.message);
-        return response.sendStatus(400);
+      let subscription;
+      let status;
+
+      switch (event.type) {
+        case "customer.subscription.created":
+          subscription = event.data.object;
+          status = subscription.status;
+          console.log(`Subscription status is ${status}.`);
+          break;
+        default:
+          console.log(`Unhandled event type ${event.type}.`);
       }
+
+      response.status(200).send("Event received");
+    } catch (err) {
+      console.log(`⚠️  Webhook signature verification failed.`, err.message);
+      response.status(400).send(`Webhook Error: ${err.message}`);
     }
-
-    let subscription;
-    let status;
-
-    switch (event.type) {
-      case "customer.subscription.created":
-        subscription = event.data.object;
-        status = subscription.status;
-        console.log(`Subscription status is ${status}.`);
-        break;
-      default:
-        console.log(`Unhandled event type ${event.type}.`);
-    }
-
-    response.send();
   }
 );
 
