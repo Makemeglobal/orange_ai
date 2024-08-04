f358b65fb0b8b06b76d8f433e3303c4e59295267
kundan175 <kundansingh957632@gmail.com>
2024-08-04 18:13:08 +0530

    bugs fix

--- index.js
+++ index.js
@@ -7,53 +7,94 @@ const { swaggerUi, specs } = require("./config/swagger");
 const Stripe = require("stripe");
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 const cors = require("cors");
-
 connectDB();
 const app = express();
 
-// Middleware for general JSON parsing and CORS
-app.use(cors());
-app.use(bodyParser.json()); // This is used for parsing JSON bodies for other routes
+// const corsOptions = {
+//   origin: ['https://www.poweredbyorange.ai', 'http://localhost'],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', ""],
+//   credentials: true
+// };
 
-// Webhook route
+app.use(cors());
+// app.use(cors)
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use(bodyParser.json());
+app.use("/api/auth", authRoutes);
+
 app.post(
   "/api/auth/webhook",
-  express.raw({ type: "application/json" }), // Use express.raw to get the raw body for verification
+  express.raw({ type: "application/json" }),
   (request, response) => {
+    let event = request.body;
+    // Replace this endpoint secret with your endpoint's unique secret
+    // If you are testing with the CLI, find the secret by running 'stripe listen'
+    // If you are using an endpoint defined with the API or dashboard, look in your webhook settings
+    // at https://dashboard.stripe.com/webhooks
     const endpointSecret = process.env.END_POINT_SECRET;
-    const signature = request.headers["stripe-signature"];
-    console.log("Raw body:", request.body.toString());
-    try {
-      const event = stripe.webhooks.constructEvent(
-        request.body, // Raw body here
-        signature,
-        endpointSecret
-      );
-
-      let subscription;
-      let status;
-
-      switch (event.type) {
-        case "customer.subscription.created":
-          subscription = event.data.object;
-          status = subscription.status;
-          console.log(`Subscription status is ${status}.`);
-          break;
-        default:
-          console.log(`Unhandled event type ${event.type}.`);
+    // Only verify the event if you have an endpoint secret defined.
+    // Otherwise use the basic event deserialized with JSON.parse
+    if (endpointSecret) {
+      // Get the signature sent by Stripe
+      const signature = request.headers["stripe-signature"];
+      try {
+        event = stripe.webhooks.constructEvent(
+          request.body,
+          signature,
+          endpointSecret
+        );
+      } catch (err) {
+        console.log(`⚠️  Webhook signature verification failed.`, err.message);
+        return response.sendStatus(400);
       }
-
-      response.status(200).send("Event received");
-    } catch (err) {
-      console.log(`⚠️  Webhook signature verification failed.`, err.message);
-      response.status(400).send(`Webhook Error: ${err.message}`);
     }
+    let subscription;
+    let status;
+    // Handle the event
+    switch (event.type) {
+      case "customer.subscription.trial_will_end":
+        subscription = event.data.object;
+        status = subscription.status;
+        console.log(`Subscription status is ${status}.`);
+        // Then define and call a method to handle the subscription trial ending.
+        // handleSubscriptionTrialEnding(subscription);
+        break;
+      case "customer.subscription.deleted":
+        subscription = event.data.object;
+        status = subscription.status;
+        console.log(`Subscription status is ${status}.`);
+        // Then define and call a method to handle the subscription deleted.
+        // handleSubscriptionDeleted(subscriptionDeleted);
+        break;
+      case "customer.subscription.created":
+        subscription = event.data.object;
+        status = subscription.status;
+        console.log(`Subscription status is ${status}.`);
+        // Then define and call a method to handle the subscription created.
+        // handleSubscriptionCreated(subscription);
+        break;
+      case "customer.subscription.updated":
+        subscription = event.data.object;
+        status = subscription.status;
+        console.log(`Subscription status is ${status}.`);
+        // Then define and call a method to handle the subscription update.
+        // handleSubscriptionUpdated(subscription);
+        break;
+      case "entitlements.active_entitlement_summary.updated":
+        subscription = event.data.object;
+        console.log(`Active entitlement summary updated for ${subscription}.`);
+        // Then define and call a method to handle active entitlement summary updated
+        // handleEntitlementUpdated(subscription);
+        break;
+      default:
+        // Unexpected event type
+        console.log(`Unhandled event type ${event.type}.`);
+    }
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
   }
 );
-
-app.use("/api/auth", authRoutes);
-// Uncomment if using Swagger
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
