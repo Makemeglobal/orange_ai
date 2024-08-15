3955d0c65739528667cd9de91cdcf9694750a91c
Harsh <harshvchawla997@gmail.com>
2024-08-15 19:16:48 +0530

    fixing invite

--- controller/authController.js
+++ controller/authController.js
@@ -252,9 +252,7 @@ exports.acceptInvitation = async (req, res) => {
       // `http://localhost:3000/signup?token=${savedToken}`,
       `https://www.poweredbyorange.ai/signup?token=${savedToken}&email=${email}`
     );
-    res
-      .status(201)
-      .json({ message: "User created successfully and added as sub-user" });
+   
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: "Server error" });
